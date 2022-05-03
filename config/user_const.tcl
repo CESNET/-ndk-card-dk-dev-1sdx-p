d295851171d3302e6b56363c1173e488e65f8103
Jakub Cabal <cabal@cesnet.cz>
2022-05-03 14:50:08 +0200

    CARD: [FEATURE] Add PCIE_GEN parameter

--- config/user_const.tcl
+++ config/user_const.tcl
@@ -33,17 +33,29 @@ set ETH_PORT_SPEED(1) $env(ETH_PORT_SPEED)
 set ETH_PORT_CHAN(0) $env(ETH_PORT_CHAN)
 set ETH_PORT_CHAN(1) $env(ETH_PORT_CHAN)
 
-# PCIe parameters:
-# ================
-# PCIe endpoints (allowed values: 1, 2, 4):
+# ==============================================================================
+# PCIe parameters (not all combinations work):
+# ==============================================================================
+# Supported combinations for this card:
+# 1x PCIe Gen4 x8x8 -- PCIE_GEN=4, PCIE_ENDPOINTS=2, PCIE_ENDPOINT_MODE=1 (Note: default configuration)
+# 1x PCIe Gen4 x16  -- PCIE_GEN=4, PCIE_ENDPOINTS=1, PCIE_ENDPOINT_MODE=0 (Note: worse DMA performance)
+# 2x PCIe Gen4 x8x8 -- PCIE_GEN=4, PCIE_ENDPOINTS=4, PCIE_ENDPOINT_MODE=1 (Note: only for DMA_400G_DEMO)
+# ------------------------------------------------------------------------------
+# PCIe Generation (possible values: 3, 4, 5):
+# 3 = PCIe Gen3
+# 4 = PCIe Gen4 (Stratix 10 with P-Tile or Agilex)
+# 5 = PCIe Gen5 (Agilex with R-Tile)
+set PCIE_GEN           4
+# PCIe endpoints (possible values: 1, 2, 4):
 # 1 = 1x PCIe x16 in one slot
 # 2 = 2x PCIe x16 in two slot OR 2x PCIe x8 in one slot (bifurcation x8+x8)
 # 4 = 4x PCIe x8 in two slots (bifurcation x8+x8)
 set PCIE_ENDPOINTS     2
-# PCIe endpoint mode (allowed values: 0, 1):
+# PCIe endpoint mode (possible values: 0, 1):
 # 0 = 1x16 lanes
 # 1 = 2x8 lanes (bifurcation x8+x8)
 set PCIE_ENDPOINT_MODE 1
+# ------------------------------------------------------------------------------
 
 # DMA parameters:
 # ===============
