2c6613342108bb5b3ea733cf0463efe35f84e89e
Jakub Cabal <cabal@cesnet.cz>
2022-08-12 15:35:18 +0200

    CARD: [MAINTENANCE] Clean card top-level

--- src/fpga.vhd
+++ src/fpga.vhd
@@ -264,11 +264,6 @@ begin
         PCIE_CLKS               => PCIE_CLKS,
         PCIE_CONS               => PCIE_CONS,
 
-        PCI_VENDOR_ID           => X"1B26",
-        PCI_DEVICE_ID           => X"C400",
-        PCI_SUBVENDOR_ID        => X"1B26",
-        PCI_SUBDEVICE_ID        => X"0800",
-
         ETH_CORE_ARCH           => NET_MOD_ARCH,
         ETH_PORTS               => ETH_PORTS,
         ETH_PORT_SPEED          => ETH_PORT_SPEED,
