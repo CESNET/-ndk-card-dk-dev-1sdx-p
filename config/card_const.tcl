6168266639d19c4a10bbf8a566cf0c80cf1bac4e
Vladislav Valek <vladislav.valek@email.cz>
2022-10-06 14:47:15 +0200

    card_const [FEATURE]: removed code duplicates
    Those path settings and sourcing of various files has now been moved to
    core_boostrap.tcl file which does this job without being tied to current
    card repository.

--- config/card_const.tcl
+++ config/card_const.tcl
@@ -1,22 +1,13 @@
-# ndk_const.tcl: Base parameters for DK-DEV-1SDX-P card
-# Copyright (C) 2020 CESNET, z. s. p. o.
+# card_const.tcl: Default parameters for DK-DEV-1SDX-P card (developement only)
+# Copyright (C) 2022 CESNET, z. s. p. o.
 # Author(s): Jakub Cabal <cabal@cesnet.cz>
+#            Vladislav Valek <valekv@cesnet.cz>
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-set OFM_PATH        $env(OFM_PATH)
-set COMBO_BASE      $env(COMBO_BASE)
-set FIRMWARE_BASE   $env(FIRMWARE_BASE)
-set CARD_BASE       $env(CARD_BASE)
-set NDK_CONST       $env(NDK_CONST)
-set DEFAULT_CONST   $env(DEFAULT_CONST)
-set USER_CONST      $env(USER_CONST)
-set OUTPUT_NAME     $env(OUTPUT_NAME)
-
-set CARD_COMMON_BASE $COMBO_BASE/ndk/core/intel
-
-source $OFM_PATH/build/VhdlPkgGen.tcl
-source $OFM_PATH/build/Shared.tcl
+# WARNING: The user should not deliberately change parameters in this file. For
+# the description of this file, visit the Parametrization section in the
+# documentation of the NDK-CORE repostiory
 
 set CARD_NAME "DK-DEV-1SDX-P"
 # Achitecture of Clock generator (INTEL or USP)
@@ -28,14 +19,6 @@ set NET_MOD_ARCH "E_TILE"
 # Achitecture of SDM/SYSMON module
 set SDM_SYSMON_ARCH "S10_ADC"
 
-VhdlPkgBegin
-
-# Source default constants
-source $DEFAULT_CONST
-
-# Source application constants if they exists
-if {$USER_CONST ne ""} {
-	source $USER_CONST
 # ------------------------------------------------------------------------------
 # ETH parameters:
 # ------------------------------------------------------------------------------
@@ -73,8 +56,5 @@ if {$ETH_PORT_SPEED(0) == 10} {
 	# 100GE and 25GE in E-Tile
     set TSU_FREQUENCY 402832031
 }
-
-# Source common core const scripts
-source $CARD_COMMON_BASE/config/ndk_const.tcl
 
 VhdlPkgBool DMA_400G_DEMO $DMA_400G_DEMO
